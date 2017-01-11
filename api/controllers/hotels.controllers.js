456308987526fe5515e5300bde829e1be106ef3d
acuodancer <leminhduc74@gmail.com>
2017-01-11 17:50:52 +0800

    Add method to get hotel by Id to HotelControllers

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,8 +1,19 @@
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
-  console.log("GET the json");
+  console.log("GET the hotels");
   res
     .status(200)
     .json(  hotelData );
+};
+
+module.exports.hotelsGetOne = function(req, res) {
+  // get info from requset
+  var hotelId = req.params.hotelId;
+  // get the desired json
+  var thisHotel = hotelData[hotelId];
+  console.log("GET hotel: " + hotelId);
+  res
+    .status(200)
+    .json(  thisHotel );
 };
