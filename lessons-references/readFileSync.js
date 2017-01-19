3edf486fb481e1caaea184f7d44604acca1e7a9a
acuodancer <leminhduc74@gmail.com>
2017-01-19 15:21:24 +0800

    remove unrelated documents

--- lessons-references/readFileSync.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs'); // default file system class
-console.log("Going to get a file");
-var file = fs.readFileSync('readFileSync.js'); // real database will cause delay
-console.log("Got the file");
-console.log("App continues...");
