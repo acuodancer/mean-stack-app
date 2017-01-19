4efe2ea10fc4160df15ab7d5b82465b1e74702d9
acuodancer <leminhduc74@gmail.com>
2017-01-19 15:26:22 +0800

    Add controller for find one review

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -4,12 +4,13 @@ var Hotel = mongoose.model('Hotel');
 module.exports.reviewsGetAll = function(req, res) {
   // get info from requset
   var hotelId = req.params.hotelId;
-  console.log("GET htelId ", hotelId);
+  console.log("GET hotelId ", hotelId);
 
   Hotel
     .findById(hotelId)
     .select('reviews')
     .exec(function(err, doc) {
+      console.log("Returned doc ", doc);
       res
         .status(200)
         .json(doc.reviews);
@@ -17,5 +18,10 @@ module.exports.reviewsGetAll = function(req, res) {
 }
 // GET a single review for one hotel
 module.exports.reviewsGetOne = function(req, res) {
-
+  var hotelId = req.params.hotelId;
+  var reviewId = req.params.reviewId;
+  console.log("Returned doc", doc);
+  res
+    .stqatus(200)
+    .json(doc.reviews);
 }
